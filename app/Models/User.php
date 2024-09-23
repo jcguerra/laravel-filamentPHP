618f46587299b93230391c690cdd7521be9eb2d6
Juan Guerra <juancarlosguerra46@gmail.com>
2024-09-23 19:02:33 -0300

    added Admin middleware

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,6 +25,7 @@ class User extends Authenticatable implements HasTenants
         'name',
         'email',
         'password',
+        'is_admin',
     ];
 
     /**
@@ -47,6 +48,7 @@ class User extends Authenticatable implements HasTenants
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'is_admin' => 'boolean',
         ];
     }
 
@@ -64,4 +66,5 @@ class User extends Authenticatable implements HasTenants
     {
         return $this->teams()->whereKey($tenant)->exists();
     }
+
 }
